f15c3cb59c4c89fc491b6e2376eb71b0d0412bcc
Saki Ebina <ebiebi0u0@gmail.com>
2024-07-23 16:30:09 -0700

    finished

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,26 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  console.log(data1)
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    console.log(data2)
+
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      console.log(data3)
+
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        console.log(data4)
+
+        const hobbiesArray = JSON.parse(data4);
+        const firstHobby = hobbiesArray[0];
+        const secondHobby = hobbiesArray[1];
+
+        const sentence = `${data1} ${data2} is ${data3} years old and his hobbies are ${firstHobby} and ${secondHobby}`;
+        console.log(sentence);
+      })
+    })
+  })
 })
+
+
+
